5b9cc619474cc0363cabc24fda61c8d77fbd6f54
Alan <alan.mesquita@dafiti.com.br>
2013-09-04 12:26:25 -0300

    Jasmine dojo finished

--- 2013-08-30/spec/AtmSpec.js
+++ 2013-08-30/spec/AtmSpec.js
@@ -8,7 +8,43 @@ describe('ATM', function() {
         expect(atm.withdraw(10)).toEqual({'10':1});
     });
 
-    it("atm should receive 20 and return 20", function(){
+    it("atm should receive 20 and return 1x20", function(){
         expect(atm.withdraw(20)).toEqual({'20':1});
+    });
+
+    it("atm should receive 50 and return 1x50", function(){
+        expect(atm.withdraw(50)).toEqual({'50':1});
+    });
+
+    it("atm should receive 100 and return 1x100", function(){
+        expect(atm.withdraw(100)).toEqual({'100':1});
+    });
+
+    it("atm should receive 30 and return 1x20 and 1x10", function(){
+        expect(atm.withdraw(30)).toEqual({'20':1, '10':1});
+    });
+
+    it("atm should receive 40 and return 2x20", function(){
+        expect(atm.withdraw(40)).toEqual({'20':2});
+    });
+
+    it("atm should receive 60 and return 1x50 and 1x10", function(){
+        expect(atm.withdraw(60)).toEqual({'50':1,'10':1});
+    });
+
+    it("atm should receive 70 and return 1x50 and 1x20", function(){
+        expect(atm.withdraw(70)).toEqual({'50':1,'20':1});
+    });
+
+    it("atm should receive 80 and return 1x50, 1x20 and 1x10", function(){
+        expect(atm.withdraw(80)).toEqual({'50':1,'20':1, '10':1});
+    });
+    
+    it("atm should receive 90 and return 1x50, 2x20", function(){
+        expect(atm.withdraw(90)).toEqual({'50':1,'20':2});
+    });
+
+    it("atm should receive 180 and return 1x100, 1x50, 1x20, 1x10", function(){
+        expect(atm.withdraw(180)).toEqual({'100':1,'50':1,'20':1,'10':1});
     });
 });
--- 2013-08-30/src/Atm.js
+++ 2013-08-30/src/Atm.js
@@ -1,8 +1,17 @@
 var Atm = function() {
-    this.bills = [10, 20];
+    this.bills = [100, 50, 20, 10];
 
     this.withdraw = function(amount) {
-        return {'10':1};
+        var result = {}, numberOfBills;
+
+        for (i in this.bills) {
+            numberOfBills = Math.floor(amount/this.bills[i]);
+            if (numberOfBills > 0) {
+                result[this.bills[i]] = numberOfBills;
+                amount -= this.bills[i]*numberOfBills;
+            }
+        }
+        return result;
     };
 };
 
