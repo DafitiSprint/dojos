02cbe5bda90e132d74e329db976bbb6d07e751c6
Luiz Guilherme da Silva Junior <luizguilherme@recebedor.com>
2014-09-09 02:02:32 -0300

    Dojo Jasmine BDD - ImageFeed

---
+++ 2014-09-09/spec/ImageFeedSpec.js
@@ -0,0 +1,16 @@
+describe("Image Feed", function(){
+    
+    it("is a jQuery Plugin");
+
+    it("has it own scope, so can't be instantiated outside it");
+
+    it("lazyload a plugin instance on data-image-feed click");
+
+    it("is not loaded without a click");
+
+    it("loads only one plugin instance per data-image-feed");
+
+    it("reads a remote feed from data-remote url");
+
+    it("create img elements to show remote feed images");
+})
