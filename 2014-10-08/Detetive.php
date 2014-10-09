dd535f74054e5c7f91c83f342dc64fc9f7557728
Pedro Ribeiro <pcelta.ribeiro@gmail.com>
2014-10-08 23:11:01 -0300

    basic usage ok

--- 2014-10-08/Detetive.php
+++ 2014-10-08/Detetive.php
@@ -1,46 +1,59 @@
 <?php
 
-class Detetive {
+class Detetive
+{
 
-	private $list = array();
-	private $counter = [];
-	public function __construct($list = array())
-	{
-		$this->list = $list;
-	}
-	
-	public function search()
-	{
-		if (!count($this->list)) {
-			return null;
-		}
+    private $list = [];
+    private $counter = [];
 
-		if (count($this->list) == 1) {
-			return $this->list[0];
-		}
+    public function __construct(array $list)
+    {
+        $this->list = $list;
+    }
+    
+    public function search()
+    {
+        if (empty($this->list)) {
 
-		foreach ($this->list as $item)
-		{
-			if (!isset($this->counter[$item])) {
-				$this->counter[$item] = 1;
-			} else {
-				$this->counter[$item]++;
-			}
-		}
+            return null;
+        }
 
+        if (count($this->list) == 1) {
 
-		$lowest = null;
-		
-		foreach($this->counter as $key => $value) {
-			if ($lowest < $value) {
-				$lowest = $key;
-			}
+            return reset($this->list);
+        }
 
-			
-		}
+        foreach ($this->list as $item) {
+            if (!array_key_exists($item, $this->counter)) {
+                $this->counter[$item] = 1;
+            } else {
+                $this->counter[$item]++;
+            }
+        }
 
-		return $lowest;
-	}
+        $lowest["item"] = key($this->counter);
+        $lowest["count"] = $this->counter[$lowest["item"]];
+
+        foreach ($this->counter as $key => $value) {
+            if ($lowest["item"] == $key) {
+
+                continue;
+            }
+
+            if ($lowest["count"] < $value) {
+                $lowest["count"] = $value;
+                $lowest["item"] = $key;
+
+                continue;
+            }
+        }
+
+        if (count(array_keys($this->counter, $lowest["count"])) > 1) {
+            return null;
+        };
+
+        return $lowest;
+    }
 
 
 }
--- 2014-10-08/DetetiveTest.php
+++ 2014-10-08/DetetiveTest.php
@@ -4,30 +4,30 @@
 include "Detetive.php";
 
 class DetetiveTest extends PHPUnit_Framework_TestCase{
-	
+    
 
-	public function testShouldReturnOnlyElement()
-	{
-		$expected = 1;
-		$list = [1];
+    public function testShouldReturnOnlyElement()
+    {
+        $expected = 1;
+        $list = [1];
 
-		$detetive = new Detetive($list);
-		$this->assertEquals($expected, $detetive->search());
-	}
+        $detetive = new Detetive($list);
+        $this->assertEquals($expected, $detetive->search());
+    }
 
-	public function testShouldReturnNullIfInputIsEmptyArray()
-	{	
-		$expected = null;
-		$detetive = new Detetive();
-		$this->assertEquals($expected, $detetive->search());	
-	}
+    public function testShouldReturnNullIfInputIsEmptyArray()
+    {    
+        $expected = null;
+        $detetive = new Detetive([]);
+        $this->assertEquals($expected, $detetive->search());    
+    }
 
-	public function testShouldReturnNullIfSameLowestFrequency()
-	{	
-		$expected = null;
-		$list = [1,2];
+    public function testShouldReturnNullIfSameLowestFrequency()
+    {    
+        $expected = null;
+        $list = [1,2];
 
-		$detetive = new Detetive($list);
-		$this->assertEquals($expected, $detetive->search());
-	}
+        $detetive = new Detetive($list);
+        $this->assertEquals($expected, $detetive->search());
+    }
 }
