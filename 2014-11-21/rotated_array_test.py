0e79e37c2e0d9b7dc7ac8b83846e6b3496a03835
Cavalo Power <sprint@dafiti.com.br>
2014-12-10 12:15:21 -0200

    adding solutions to binary search for rotated_array

--- 2014-11-21/rotated_array_test.py
+++ 2014-11-21/rotated_array_test.py
@@ -1,18 +1,35 @@
 import unittest
 
-from rotated_array import RotatedArray, Util
+from rotated_array import RotatedArray
 
 class RotatedArrayTest(unittest.TestCase):
 
-    def setUp(self):
-        util = Util()
-        rotated_once = util.rotate_left([1,2,3,4], 1)
-        print rotated_once
-        self.rotated_array = RotatedArray([1,2,3,4])
+    def test_should_return_2(self):
+        expected = 2
+        self.rotated_array = RotatedArray([3,4,1,2])
+        result = self.rotated_array.count_rotations()
+        self.assertEquals(expected, result)
 
-    def test_empty(self):
-        #TODO
-        expected = []
+    def test_should_return_3(self):
+        expected = 3
+        self.rotated_array = RotatedArray([2,3,4,1])
+        #result = self.rotated_array.count_rotations()
+        result = self.rotated_array.n_shifted_bs(self.rotated_array.rotated_array)
+        self.assertEquals(expected, result)
+
+    # def test_should_return_10000000_bs(self):
+    #     expected = 9995000
+    #     arr = range(5000, 10000000)
+    #     rotated = arr + range(5000)
+    #     self.rotated_array = RotatedArray(rotated)
+    #     result = self.rotated_array.n_shifted_bs(self.rotated_array.rotated_array)
+    #     self.assertEquals(expected, result)
+
+    def test_should_return_10000000_n(self):
+        expected = 9995000
+        arr = range(5000, 10000000)
+        rotated = arr + range(5000)
+        self.rotated_array = RotatedArray(rotated)
         result = self.rotated_array.count_rotations()
         self.assertEquals(expected, result)
 
